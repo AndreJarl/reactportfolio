882470e82d42463fc93163600ebdc292fd006e5c
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-07-12 10:50:06 +0800

    style css in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Projects from './components/Projects.jsx'
+import './components/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
