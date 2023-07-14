012c2a256e88569c3fc2d36a3dc7124e54f55d70
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-07-14 14:49:21 +0800

    added some project card prototype

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,48 @@
-
+import projimg from '../images/PicsArt_06-15-12.03.18.jpg'
 export default function Project(){
       return(
           <>
             <div className="projects">
                <div className="project-container">
                     <h1>MY RECENT WORKS</h1>
+                    <div className="projectCard">
+                           <div className="cardContainer">
+                           <img style={{ width: '340px'}} src={projimg} alt="" />
+                           <h3>Peaky Blinders Clothing</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nisi eveniet corrupti omnis consectetur sit? Quaerat, molestiae? Harum magnam fuga impedit modi soluta saepe architecto aspernatur totam, ad similique eligendi!</p>
+                           <div className="cardBtn">
+                                <button className='demo'>Live Demo</button>
+                                <button className='demo'>Github</button>
+                           </div>
+                           </div>
+                           <div className="cardContainer">
+                           <img style={{ width: '340px'}} src={projimg} alt="" />
+                           <h3>Peaky Blinders Clothing</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nisi eveniet corrupti omnis consectetur sit? Quaerat, molestiae? Harum magnam fuga impedit modi soluta saepe architecto aspernatur totam, ad similique eligendi!</p>
+                           <div className="cardBtn">
+                                <button className='demo'>Live Demo</button>
+                                <button className='demo'>Github</button>
+                           </div>
+                           </div>
+                           <div className="cardContainer">
+                           <img style={{ width: '340px'}} src={projimg} alt="" />
+                           <h3>Peaky Blinders Clothing</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nisi eveniet corrupti omnis consectetur sit? Quaerat, molestiae? Harum magnam fuga impedit modi soluta saepe architecto aspernatur totam, ad similique eligendi!</p>
+                           <div className="cardBtn">
+                                <button className='demo'>Live Demo</button>
+                                <button className='demo'>Github</button>
+                           </div>
+                           </div>
+                           <div className="cardContainer">
+                           <img style={{ width: '340px'}} src={projimg} alt="" />
+                           <h3>Peaky Blinders Clothing</h3>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est nisi eveniet corrupti omnis consectetur sit? Quaerat, molestiae? Harum magnam fuga impedit modi soluta saepe architecto aspernatur totam, ad similique eligendi!</p>
+                           <div className="cardBtn">
+                                <button className='demo'>Live Demo</button>
+                                <button className='demo'>Github</button>
+                           </div>
+                           </div>
+                    </div>
                </div>
             </div>
           </>
