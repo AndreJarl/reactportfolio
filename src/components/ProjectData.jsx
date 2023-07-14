5cd6d53eab47b849d8e0d4a158084b3db0247fed
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-07-14 14:55:06 +0800

    added data

---
+++ src/components/ProjectData.jsx
@@ -0,0 +1,10 @@
+import proj1 from '../images/PicsArt_06-15-12.03.18.jpg'
+
+const ProjectData = [
+    {
+        imgsrc: proj1,
+        title: "Peaky Blinders Clothing",
+        desc: "dfgsdzdfssdrfdfsdfdfdsfdff",
+        
+    }
+]
