2dab6a3d58902d5e04a3f75c8d9913a0ab453b63
AndreJarl <104331025+AndreJarl@users.noreply.github.com>
2023-07-18 16:15:43 +0800

    added project imgs

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -1,24 +1,49 @@
 import proj1 from '../assests/PicsArt_06-15-12.03.18.jpg'
 import proj2 from '../assests/PicsArt_07-26-08.24.57.jpg'
 import proj3 from '../assests/PicsArt_08-30-10.06.26.jpg'
+import proj4 from '../assests/generator.jpg'
+import proj5 from '../assests/done2.jpg'
+import proj6 from '../assests/fakeshop.jpg'
 
 const ProjectData = [
     {
         imgsrc: proj1,
-        desc: "A front-end only e-commerce website. Inspired by the BBC show Peaky Blinders.",
+        desc: "A landing page e-commerce website. Inspired by the BBC show Peaky Blinders.",
         link: "https://andrejarl.github.io/andrejarlpeakyclothing.github.io/clothing.html"
+      
         
     },
     {
         imgsrc: proj2,
-        desc: "A front-end only real-estate website.",
+        desc: "A landing page real-estate website.",
         link: "https://andrejarl.github.io/anianaluxury.github.io/index.html"
+        
     },
     {
         imgsrc: proj3,
         desc: "A tiktok viral web app that you cannot refuse.",
         link: "https://andrejarl.github.io/Anti-rejectionProposalPrank.github.io/proposal.html"
+    
+    },
+    {
+        imgsrc: proj4,
+        desc: "A Quote generator using a free public api.",
+        link: "https://andrejarl.github.io/QuotesGeneratorV2/"
+        
+    },
+    {
+        imgsrc: proj5,
+        desc: "A to do list that tracks your progress every time you done/finished a task. You gain exp for every done tasks and you level up. Only save user data to browser's local storage.",
+        link: "https://andrejarl.github.io/Done2LevelUP/gawa.html"
+        
+    },
+    {
+        imgsrc: proj6,
+        desc: "Ecommerce Store using API . With ordering cart system. Add to cart and delete cart.",
+        link: "https://andrejarl.github.io/fakeshop/commerce.html"
+        
     }
+    
 ]
 
 export default ProjectData;
