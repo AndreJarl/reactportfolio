22f4a827fb67e5df75a677c2ef71bb961ff22c1b
Andre Jarl <andrejarlaniana@gmail.com>
2025-03-17 23:34:14 +0800

    changed rotating images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,6 +14,9 @@ import figma from '../assests/figma.png'
 import visual from '../assests/visual.png'
 import git from '../assests/git.png'
 import andre from '../assests/andre.png'
+import code from '../assests/code.png'
+import data from '../assests/data.png'
+import front from '../assests/front.png'
 export default function Hero (){
     return(
         <>
@@ -22,7 +25,7 @@ export default function Hero (){
                 <div className='ccc'> 
                   <div className="lang-con">
                   <img id='me' src={andre} alt="" />
-                  <img id='me1' src={html} alt="" />
+                  {/* <img id='me1' src={html} alt="" />
                   <img id='me2' src={css} alt="" />
                   <img id='me3' src={js} alt="" />
                   <img id='me4' src={react} alt="" />
@@ -31,7 +34,10 @@ export default function Hero (){
                   <img id='me7' src={mongo} alt="" />
                   <img id='me8' src={node} alt="" />
                   <img id='me9' src={express} alt="" />
-                  <img id='me10' src={tailwind} alt="" />
+                  <img id='me10' src={tailwind} alt="" /> */}
+                  <img id='me6' src={code} alt="" />
+                  <img id='me2' src={data} alt="" />
+                  <img id='me10' src={front} alt="" />
                   </div>
                 <div className="social">
                 <a id="socs" href="https://github.com/AndreJarl"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16"> <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/> </svg>	</a>
